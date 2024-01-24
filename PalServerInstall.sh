98e91c43f6fb3e3b13614cd804174b7317c6b6e2
2lifetop <57310468+2lifetop@users.noreply.github.com>
2024-01-24 19:42:34 +0800

    Create PalServerInstall.sh

---
+++ PalServerInstall.sh
@@ -0,0 +1,232 @@
+#!/usr/bin/env bash
+#Blog:https://www.xuehaiwu.com/
+
+Green="\033[32m"
+Font="\033[0m"
+Red="\033[31m" 
+
+#root权限
+root_need(){
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${Red}Error:This script must be run as root!${Font}"
+        exit 1
+    fi
+}
+
+#检测ovz
+ovz_no(){
+    if [[ -d "/proc/vz" ]]; then
+        echo -e "${Red}Your VPS is based on OpenVZ，not supported!${Font}"
+        exit 1
+    fi
+}
+
+#检测并安装Docker
+install_docker(){
+    if command -v docker &> /dev/null; then
+        echo -e "${Green}Docker 已安装，进行下一步.${Font}"
+    else
+        echo -e "${Green}Docker 未安装，正在为您安装...${Font}"
+        curl -fsSL https://get.docker.com | bash -s docker
+        echo -e "${Green}Docker 安装成功！${Font}"
+    fi
+}
+
+#安装幻兽帕鲁服务端
+install_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Red}幻兽帕鲁服务端已存在，安装失败！${Font}"
+    else
+        echo -e "${Green}开始安装幻兽帕鲁服务端...${Font}"
+        CONTAINER_ID=$(docker run -dit --name steamcmd --net host cm2network/steamcmd)
+        docker exec -it $CONTAINER_ID bash -c "/home/steam/steamcmd/steamcmd.sh +login anonymous +app_update 2394010 validate +quit"
+        wget https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/restart.sh &&chmod +x restart.sh 
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功安装并启动！${Font}"
+    fi
+}
+
+#启动幻兽帕鲁服务端
+start_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始启动幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
+    fi
+}
+
+#停止幻兽帕鲁服务端
+stop_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始停止幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功停止！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，停止失败！${Font}"
+    fi
+}
+#查看幻兽帕鲁服务端状态
+check_pal_server_status(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}幻兽帕鲁服务端状态如下：${Font}"
+        docker ps -a -f name=steamcmd
+        echo -e "${Green}幻兽帕鲁服务端资源使用情况如下：${Font}"
+        docker stats --no-stream steamcmd
+        echo -e "${Green}服务器内存使用情况如下：${Font}"
+        free -h
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在！${Font}"
+    fi
+}
+#修改服务端配置
+modify_config(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        if [ -f ./PalWorldSettings.ini ]; then
+            echo -e "${Green}开始修改服务端配置...${Font}"
+            docker cp ./PalWorldSettings.ini steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/
+            echo -e "${Green}服务端配置已成功修改！服务端重启后生效！${Font}"
+        else
+            echo -e "${Red}未找到服务端配置文件，请前往https://www.xuehaiwu.com/Pal/进行下载。${Font}"
+        fi
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，修改配置失败！${Font}"
+    fi
+}
+
+#增加swap内存
+add_swap(){
+echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！${Font}"
+read -p "请输入swap数值:" swapsize
+
+#检查是否存在swapfile
+grep -q "swapfile" /etc/fstab
+
+#如果不存在将为其创建swap
+if [ $? -ne 0 ]; then
+    echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
+    fallocate -l ${swapsize}M /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+         echo -e "${Green}swap创建成功，并查看信息：${Font}"
+         cat /proc/swaps
+         cat /proc/meminfo | grep Swap
+else
+    echo -e "${Red}swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！${Font}"
+fi
+}
+
+#增加定时重启
+add_restart(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始增加定时重启...${Font}"
+        echo -e "${Green}1、每天凌晨5点${Font}"
+        echo -e "${Green}2、每周三凌晨5点${Font}"
+        echo -e "${Green}3、自定义${Font}"
+        read -p "请输入数字 [1-3]:" num
+        case "$num" in
+            1)
+            echo "0 5 * * * cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            2)
+            echo "0 5 * * 3 cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            3)
+            read -p "请输入定时重启的cron表达式:" cron
+            echo "$cron cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            *)
+            echo -e "${Red}请输入正确数字 [1-3]${Font}"
+            add_restart
+            ;;
+        esac
+        echo -e "${Green}定时重启已成功增加！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，增加定时重启失败！${Font}"
+    fi
+}
+
+#重启幻兽帕鲁服务端
+restart_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始重启幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功重启！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，重启失败！${Font}"
+    fi
+}
+
+#删除幻兽帕鲁服务端
+delete_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始删除幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        docker rm steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功删除！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，删除失败！${Font}"
+    fi
+}
+
+#开始菜单
+main(){
+root_need
+ovz_no
+install_docker
+clear
+echo -e "———————————————————————————————————————"
+echo -e "${Green}Linux VPS一键安装管理幻兽帕鲁服务端脚本${Font}"
+echo -e "${Green}1、安装幻兽帕鲁服务端${Font}"
+echo -e "${Green}2、启动幻兽帕鲁服务端${Font}"
+echo -e "${Green}3、停止幻兽帕鲁服务端${Font}"
+echo -e "${Green}4、修改服务端配置${Font}"
+echo -e "${Green}5、增加swap内存${Font}"
+echo -e "${Green}6、增加定时重启${Font}"
+echo -e "${Green}7、重启幻兽帕鲁服务端${Font}"
+echo -e "${Green}8、查看幻兽帕鲁服务端状态${Font}"
+echo -e "${Green}9、删除幻兽帕鲁服务端${Font}"
+echo -e "———————————————————————————————————————"
+read -p "请输入数字 [1-9]:" num
+case "$num" in
+    1)
+    install_pal_server
+    ;;
+    2)
+    start_pal_server
+    ;;
+    3)
+    stop_pal_server
+    ;;
+    4)
+    modify_config
+    ;;
+    5)
+    add_swap
+    ;;
+    6)
+    add_restart
+    ;;
+    7)
+    restart_pal_server
+    ;;
+    8)
+    check_pal_server_status
+    ;;
+    9)
+    delete_pal_server
+    ;;
+    *)
+    clear
+    echo -e "${Green}请输入正确数字 [1-9]${Font}"
+    sleep 2s
+    main
+    ;;
+    esac
+}
+main

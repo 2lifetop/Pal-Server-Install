71239b013fb726cef0e2a3097b2d7e549fa13f98
kinytu <jiawen14th@gmail.com>
2024-01-29 23:58:56 +0800

    Upgrade to v0.1.7: add backup and script check functions, and import from backup feature

--- PalServerInstall.sh
+++ PalServerInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 当前的脚本版本
-currentScriptVersion="0.1.6"
+currentScriptVersion="0.1.7"
 # 定义一些颜色和格式
 Green="\033[32m"
 Font="\033[0m"
@@ -53,7 +53,7 @@ downloadLink=$(echo $versionInfo | jq -r '.downloadLink')
 if [ ! -f version.json ]; then
     # 如果不存在，则使用从服务器获取的版本信息创建一个新的 version.json 文件
     # 将PatchVersion设置为 "0"
-    versionInfo=$(echo $versionInfo | jq '.PatchVersion = "0"')
+    versionInfo=$(echo $versionInfo | jq '.PatchVersion = "0" | .gameVersion = "0"')
     echo $versionInfo > version.json
 fi
 
@@ -114,6 +114,31 @@ install_docker(){
     fi
 }
 
+#检测重启脚本
+check_restart_script(){
+    if [ ! -f "$(pwd)/restart.sh" ]; then
+        echo -e "${Red}当前目录不包含备份脚本，正在下载...${Font}"
+        wget -O restart.sh https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/restart.sh --no-check-certificate && chmod +x restart.sh
+    fi
+}
+#运行重启脚本
+run_restart_script(){
+    echo -e "${Green}正在运行 restart.sh 脚本...${Font}"
+    ./restart.sh
+    if [ $? -eq 0 ]; then
+        echo -e "${Green}restart.sh 脚本运行成功！${Font}"
+    else
+        echo -e "${Red}restart.sh 脚本运行失败！${Font}"
+    fi
+}
+
+# 检查是否存在备份脚本
+check_backup_script(){
+    if [ ! -f "$(pwd)/backup.sh" ]; then
+        echo -e "${Red}当前目录不包含备份脚本，正在下载...${Font}"
+        wget -O backup.sh https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/backup.sh --no-check-certificate && chmod +x backup.sh
+    fi
+}
 #安装幻兽帕鲁服务端
 install_pal_server(){
     if check_docker_container; then
@@ -124,9 +149,14 @@ install_pal_server(){
         check_result "创建 Docker 容器"
         docker exec -it $CONTAINER_ID bash -c "/home/steam/steamcmd/steamcmd.sh +login anonymous +app_update 2394010 validate +quit"
         check_result "安装游戏"
-        wget -O restart.sh https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/restart.sh --no-check-certificate && chmod +x restart.sh 
-        check_result "下载 restart.sh 脚本"
-        ./restart.sh
+        # 更新本地的版本信息文件
+        jq ".gameVersion = \"$latestgameVersion\"" version.json > temp.json && mv temp.json version.json
+        echo "已更新本地的版本信息文件。"
+        check_result "更新版本信息"       
+        check_restart_script
+        check_result "检测restart.sh 脚本"
+        run_restart_script
+        check_result "运行restart.sh 脚本"
         echo -e "${Green}幻兽帕鲁服务端已成功安装并启动！${Font}"
     fi
 }
@@ -137,12 +167,16 @@ start_pal_server(){
         echo -e "${Green}开始启动幻兽帕鲁服务端...${Font}"
         docker start steamcmd
         check_result "启动 Docker 容器"
-        ./restart.sh
+        check_restart_script
+        check_result "检测restart.sh 脚本"
+        run_restart_script
+        check_result "运行restart.sh 脚本"
         echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
     else
         echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
     fi
 }
+
 
 #停止幻兽帕鲁服务端
 stop_pal_server(){
@@ -217,7 +251,9 @@ fi
 }
 #增加定时重启
 add_restart(){
-    if check_docker_container; then
+    check_restart_script
+    check_docker_container
+    if [ $? -eq 0 ]; then
         echo -e "${Green}开始增加定时重启...${Font}"
         echo -e "${Green}1、每天凌晨5点${Font}"
         echo -e "${Green}2、每周三凌晨5点${Font}"
@@ -243,7 +279,7 @@ add_restart(){
         rm mycron
         echo -e "${Green}定时重启已成功增加！${Font}"
     else
-        echo -e "${Red}幻兽帕鲁服务端不存在，增加定时重启失败！${Font}"
+        echo -e "${Red}重启脚本不存在或者幻兽帕鲁服务端不存在，增加定时重启失败！${Font}"
     fi
 }
 
@@ -251,7 +287,10 @@ add_restart(){
 restart_pal_server(){
     if check_docker_container; then
         echo -e "${Green}开始重启幻兽帕鲁服务端...${Font}"
-        ./restart.sh
+        check_restart_script
+        check_result "检测restart.sh 脚本"
+        run_restart_script
+        check_result "运行restart.sh 脚本"
         check_result "重启服务端"
         echo -e "${Green}幻兽帕鲁服务端已成功重启！${Font}"
     else
@@ -286,7 +325,10 @@ update_pal_server(){
                     # 更新本地的版本信息文件
                     jq ".gameVersion = \"$latestGameVersion\"" version.json > temp.json && mv temp.json version.json
                     echo -e "${Green}已更新本地的版本信息文件。${Font}"
-                    ./restart.sh
+                    check_restart_script
+                    check_result "检测restart.sh 脚本"
+                    run_restart_script
+                    check_result "运行restart.sh 脚本"
                     echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
                 else
                     echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
@@ -318,7 +360,7 @@ update_patch_version() {
         echo "已经是最新优化补丁，无需更新。"
     else
         # 询问用户是否要更新至最新版补丁
-        read -p "目前linux有两个版本的服务器，本补丁是基于Palworld Dedicated Server (ID：2394010 )，请先核实是否一致，如果不懂请勿更新。新的补丁版本可用，版本为 $latestPatchVersion。是否要更新至最新版补丁? (y/n) " answer
+        read -p "目前，在游戏的1.3.1版本的服务端中， 内存泄漏的部分已经被官方修复，目前部分系统中占用过多内存的问题仅仅是由于分配了过多的对象，现在无需再打补丁，后续会出Linux优化版，所以暂时未移除这个功能。最新版本为 $latestPatchVersion。是否要更新至最新版补丁? (y/n) " answer
         case ${answer:0:1} in
             y|Y )
                 # 下载最新的补丁版本
@@ -345,14 +387,17 @@ update_patch_version() {
 #导入幻兽帕鲁存档及配置
 import_pal_server(){
     if check_docker_container; then
-        read -p "请将幻兽帕鲁存档及配置(Saved)文件夹放入 /data/palworld 目录，然后回车继续" import
+        read -p "请确认已将幻兽帕鲁存档及配置(Saved)文件夹放入 /data/palworld 目录，然后回车继续" import
         echo -e "${Green}开始导入幻兽帕鲁存档及配置...${Font}"
         chmod -R 777 /data/palworld/
+        docker restart steamcmd
         docker cp -a /data/palworld/Saved/ steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/
         check_result "导入存档及配置"
         echo -e "${Green}开始重启幻兽帕鲁服务端...${Font}"
-        docker restart steamcmd
-        ./restart.sh
+        check_restart_script
+        check_result "检测restart.sh 脚本"
+        run_restart_script
+        check_result "运行restart.sh 脚本"
         check_result "重启服务端"
         echo -e "${Green}幻兽帕鲁服务端已成功重启！${Font}"
         echo -e "${Green}幻兽帕鲁存档及配置已成功导入！${Font}"
@@ -361,6 +406,35 @@ import_pal_server(){
     fi
 }
 
+import_from_backup(){
+    echo "你想要从备份压缩包导入吗？否则将直接从 /data/palworld/Saved 中导入。"
+    select yn in "是" "否" "取消"; do
+        case $yn in
+            是 )
+                echo "选择一个备份文件："
+                select backup in /data/backup/*.tar.gz; do
+                    echo "你选择了备份文件 $backup"
+                    echo "开始删除 /data/palworld/ 目录下的所有文件和子目录..."
+                    mkdir -p /data/palworld
+                    rm -rf /data/palworld/*
+                    echo "开始解压备份文件..."
+                    tar -xzf "$backup" --strip-components=1 -C /data/palworld/
+                    check_result "解压备份文件"
+                    echo "备份文件已成功解压到 /data/palworld/ 目录！"
+                    break
+                done
+                import_pal_server
+                break;;
+            否 )
+                echo "将直接从 /data/palworld/Saved 中导入备份..."
+                import_pal_server
+                break;;
+            取消 )
+                echo "操作已取消。"
+                break;;
+        esac
+    done
+}
 # 导出幻兽帕鲁存档及配置
 export_pal_server() {
     if check_docker_container; then
@@ -373,6 +447,41 @@ export_pal_server() {
         echo -e "${Green}幻兽帕鲁存档及配置已成功导出！${Font}"
     else
         echo -e "${Red}幻兽帕鲁服务端不存在，导出失败！${Font}"
+    fi
+}
+
+
+# 增加定时备份
+add_backup(){
+    check_backup_script
+    check_docker_container
+    if [ $? -eq 0 ]; then
+        echo -e "${Green}开始增加定时备份...${Font}"
+        echo -e "${Green}1、每天凌晨5点${Font}"
+        echo -e "${Green}2、每周三凌晨5点${Font}"
+        echo -e "${Green}3、每多少小时备份一次${Font}"
+        read -p "请输入数字 [1-3]:" num
+        case "$num" in
+            1)
+            echo "0 5 * * * /bin/bash $(pwd)/backup.sh >> $(pwd)/crontab.log" > mycron
+            ;;
+            2)
+            echo "0 5 * * 3 /bin/bash $(pwd)/backup.sh >> $(pwd)/crontab.log" > mycron
+            ;;
+            3)
+            read -p "请输入每多少小时备份一次:" hours
+            echo "0 */$hours * * * /bin/bash $(pwd)/backup.sh >> $(pwd)/crontab.log" > mycron
+            ;;
+            *)
+            echo -e "${Red}请输入正确数字 [1-3]${Font}"
+            add_backup
+            ;;
+        esac
+        crontab mycron
+        rm mycron
+        echo -e "${Green}定时备份已成功增加！${Font}"
+    else
+        echo -e "${Red}备份脚本不存在或者服务端未安装，增加定时备份失败！${Font}"
     fi
 }
 
@@ -401,7 +510,8 @@ echo -e "${Green}11、删除幻兽帕鲁服务端${Font}"
 echo -e "${Green}12、更新补丁版本${Font}"
 echo -e "${Green}13、导入幻兽帕鲁存档及配置${Font}"
 echo -e "${Green}14、导出幻兽帕鲁存档及配置${Font}"
-echo -e "${Green}15、退出脚本${Font}"
+echo -e "${Green}15、增加定时备份幻兽帕鲁存档及配置${Font}"
+echo -e "${Green}16、退出脚本${Font}"
 echo -e "———————————————————————————————————————"
 read -p "请输入数字 [1-15]:" num
 check_numeric_input $num
@@ -443,12 +553,15 @@ case "$num" in
     update_patch_version
     ;;
     13)
-    import_pal_server
+    import_from_backup
     ;;
     14)
     export_pal_server
     ;;
     15)
+    add_backup
+    ;;
+    16)
     echo -e "${Green}退出脚本...${Font}"
     exit 0
     ;;
